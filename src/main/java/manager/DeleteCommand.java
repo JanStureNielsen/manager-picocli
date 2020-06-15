c9eb57ac61a668cbcdd0463bbc0595f7d44e9914
Jan Nielsen <jan.sture.nielsen@gmail.com>
2020-06-15 12:36:43 -0600

    Remove unused header annotation attribute.

--- src/main/java/manager/DeleteCommand.java
+++ src/main/java/manager/DeleteCommand.java
@@ -6,7 +6,6 @@ import picocli.CommandLine.Command;
 
 @Command(
     name = "delete",
-    header = "delete stuff",
     description = "This is the long description, decribing in great detail what, how, and why it deletes stuff.",
     mixinStandardHelpOptions = true)
 @Component
