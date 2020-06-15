e2f279095cb0aa33567c49426bed32ad9b4533de
Jan Nielsen <jan.sture.nielsen@gmail.com>
2020-06-15 15:35:19 -0600

    Polish

--- src/main/java/manager/ConnectCommand.java
+++ src/main/java/manager/ConnectCommand.java
@@ -24,7 +24,7 @@ import picocli.CommandLine.Option;
 @Component @RequiredArgsConstructor
 public class ConnectCommand implements Runnable, ExitCodeGenerator {
     private final @NotNull IFactory picocliFactory;
-	private final @NotNull ShellCommand shell;
+    private final @NotNull ShellCommand shell;
 
     @Option(
         names        = {"-u", "--username"},
